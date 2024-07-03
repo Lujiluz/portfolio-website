0e81bf6ee478bcc12b4af57a9f77b53a4beafa2e
Zulfajri <93115572+Lujiluz@users.noreply.github.com>
2024-07-03 13:21:37 +0700

    fix: message sent when form is not completed

--- static/script.js
+++ static/script.js
@@ -1,6 +1,14 @@
 const send_message = () => {
   let person_sender = $('#person_sender').val();
   let message = $('#message').val();
+
+  if (person_sender == '' || message == '') {
+    return Swal.fire({
+      title: 'Cannot send message! ❌',
+      text: 'Please complete the form 🙏',
+      icon: 'warning',
+    });
+  }
   $.ajax({
     type: 'POST',
     url: '/contact',
